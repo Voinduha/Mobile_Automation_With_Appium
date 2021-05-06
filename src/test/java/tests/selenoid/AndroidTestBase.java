9175e993d1e05e0c6190b452cf47d2a5519f4b8c
DanVu <danvu@ya.ru>
2021-05-06 17:54:24 +0300

    added selenoid test

--- src/test/java/tests/selenoid/AndroidTestBase.java
+++ src/test/java/tests/selenoid/AndroidTestBase.java
@@ -1,7 +1,8 @@
-package tests.local;
+package tests.selenoid;
 
 import com.codeborne.selenide.Configuration;
 import drivers.LocalMobileDriver;
+import drivers.SelenoidMobileDriver;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,7 +13,8 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
-import static helpers.AttachmentsHelper.*;
+import static helpers.AttachmentsHelper.attachPageSource;
+import static helpers.AttachmentsHelper.attachScreenshot;
 import static io.qameta.allure.Allure.step;
 
 public class AndroidTestBase {
@@ -20,12 +22,15 @@ public class AndroidTestBase {
     @BeforeAll
     public static void beforeAll() {
         addListener("AllureSelenide", new AllureSelenide());
-        Configuration.browser = LocalMobileDriver.class.getName();
+        Configuration.browser = SelenoidMobileDriver.class.getName();
         Configuration.startMaximized = false;
         Configuration.browserSize = null;
         Configuration.timeout = 1000;
     }
 
+    public static String getSessionId() {
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+    }
 
     @BeforeEach
     void beforeEach() {
@@ -34,19 +39,9 @@ public class AndroidTestBase {
 
     @AfterEach
     public void afterEach() {
-        String sessionId = getSessionId();
-
         attachScreenshot("Last screenshot");
         attachPageSource();
 
         closeWebDriver();
-
-        if (System.getProperty("video.url") != "")
-            attachVideo(sessionId);
-
-    }
-
-    public static String getSessionId() {
-        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
     }
 }
